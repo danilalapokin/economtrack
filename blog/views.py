b1f12513ec162c9ddb964a37441f99040ecdcfd9
danilalapokin <danilalapokin@gmail.com>
2020-06-25 11:58:38 +0400

    EconomTrack project

--- blog/views.py
+++ blog/views.py
@@ -2,5 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def post_list(request):
-    data = {"header": "Welcome to Economtrack", "message": "It's Liza and Daniel's project", "bgcol": "FF00FF"}
-    return render(request, 'blog/post_list.html', context=data)
+    return render(request, 'blog/post_list.html', {})
