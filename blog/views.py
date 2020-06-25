f4ddd38bbe97a31e848ca89c4c8bb8c2f601fc20
danilalapokin <danilalapokin@gmail.com>
2020-06-25 12:10:41 +0400

    economtrack

--- blog/views.py
+++ blog/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
+
+def about(request):
+    return HttpResponse("<h2>About site</h2>")
