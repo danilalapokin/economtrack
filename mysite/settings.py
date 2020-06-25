a13589589dbf24784ac5f928c6e61506943edeea
danilalapokin <danilalapokin@gmail.com>
2020-06-25 12:02:10 +0400

    economtrack

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&f=%#_9p+2%^t4jl#^p-tbuy^s&afd-ejfbcc!r)68#q1nmg@5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
