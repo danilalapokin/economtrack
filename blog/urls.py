de56966d9c3a7c590985fb1d5a0d154d757595fa
danilalapokin <danilalapokin@gmail.com>
2020-06-28 22:54:02 +0400

    danila

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('/game', views.game, name='game'),
+
     path('about', views.about)
 ]
